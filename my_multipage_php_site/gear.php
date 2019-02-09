4ab358e4adaad6819322a091a9c0dd40349812b6
Austin Malmberg <austin.malmberg@gmail.com>
2019-02-09 02:24:03 -0500

    Created gear.php page

---
+++ my_multipage_php_site/gear.php
@@ -0,0 +1,93 @@
+<main class="lightened">
+      
+  <div class="wrapper">
+
+    <h1>Gear</h1>
+
+    <p>Just like being outdoors, when it comes to the cost of equipment, the sky is the limit. As a rule of thumb, weight is indrectly proportional to cost, or rather, the more expensive it is, the lighter it will be. As you might expect, expensive gear also tends to last longer as well.</p>
+
+    <p>While picking out my gear, I wanted to balance price and quality. Light gear is great, but I'm in decent shape so this was low on my totem pole of concerns.</p>
+
+    <section class="gear" id="tent">
+
+      <h2>Tent: <a href="https://www.rei.com/product/141933/msr-hubba-hubba-nx-2-tent" target="_blank">MSR Hubba Hubba NX 2</a></h2>
+
+      <p>Deal of a lifetime. Retails for $450. REI garage sale: $81</p>
+      
+      <figure class="sprite gearsheet" id="tent-fig" src="../images/placeholder-pixel.png" alt="gearsheet" height="1px" width="1px"></figure>
+
+    </section>
+
+    <section class="gear" id="backpack">
+
+      <h2>Backpack: <a href="https://www.amazon.com/gp/product/B01JYY7M5S/" target="_blank">Teton Sports Expolorer 4000</a></h2>
+
+      <p>Was invited by my buddy to do an overnight at South Mountain and didn't have a backpack. I did have Amazon Prime though. One of my camping first purchases. Amazon: $55</p>
+
+      <figure class="sprite gearsheet" id="backpack-fig" src="../images/placeholder-pixel.png" alt="gearsheet" height="1px" width="1px"></figure>
+
+    </section>
+
+    <section class="gear" id="sleeping-bag">
+
+      <h2>Sleeping Bag: <a href="https://www.rei.com/product/127386" target="_blank">REI Trail Pod 30</a></h2>
+
+      <p>Invaluable in the fall or a warm winter. My first REI purchase: $44</p>
+
+      <figure class="sprite gearsheet" id="sleepingbag-fig" src="../images/placeholder-pixel.png" alt="gearsheet" height="1px" width="1px"></figure>
+
+    </section>
+
+    <section class="gear" id="sleeping-pad">
+
+      <h2>Sleeping Pad: <a href="https://www.rei.com/product/870759/rei-co-op-trekker-self-inflating-sleeping-pad" target="_blank">REI Trekker Self-Inflating</a></h2>
+
+      <p>Not really meant for hiking because it's not all that light and can't be compressed. But it is comfy...and I caught it on sale. REI: $48</p>
+
+      <figure class="sprite gearsheet" id="sleepingpad-fig" src="../images/placeholder-pixel.png" alt="gearsheet" height="1px" width="1px"></figure>
+
+    </section>
+
+    <section class="gear" id="stove">
+
+      <h2>Stove: <a href="https://www.rei.com/product/114890/msr-pocketrocket-2-stove" target="_blank">MSR PocketRocket 2</a></h2>
+
+      <p>Had a 20% off coupon from REI and couldn't resist. REI: $36</p>
+
+      <figure class="sprite gearsheet" id="stove-fig" src="../images/placeholder-pixel.png" alt="gearsheet" height="1px" width="1px"></figure>
+
+    </section>
+
+    <section class="gear" id="cookset">
+
+      <h2>Cookset: <a href="https://www.rei.com/product/870075/snow-peak-personal-cooker-3-cookset">Snow Peak Personal Cooker 3 Cookset</a></h2>
+
+      <p>Bought with the stove. They aren't titanium, but they're a helluvalot cheaper. REI: $24</p>
+
+      <figure class="sprite gearsheet" id="cookset-fig" src="../images/placeholder-pixel.png" alt="gearsheet" height="1px" width="1px"></figure>
+
+    </section>
+
+    <section class="gear" id="jacket">
+
+      <h2>Jacket: <a href="https://www.rei.com/product/137433/rei-co-op-magma-850-down-hoodie-mens">REI Magma 850 Down Hoodie</a></h2>
+
+      <p>Unbelievable warm for its weight. Bought on sale. REI: $108</p>
+
+      <figure class="sprite gearsheet" id="down-jacket-fig" src="../images/placeholder-pixel.png" alt="gearsheet" height="1px" width="1px"></figure>
+
+    </section>
+
+    <section class="gear" id="waterproof-jacket">
+
+      <h2>Waterproof Jacket: <a href="https://www.rei.com/rei-garage/product/143222/outdoor-research-helium-ii-jacket-mens">Outdoor Research Helium II</a></h2>
+
+      <p>Packable. Too make rainy days more bearable. Bought on sale. REI: $89</p>
+
+      <figure class="sprite gearsheet" id="waterproof-jacket-fig" src="../images/placeholder-pixel.png" alt="gearsheet" height="1px" width="1px"></figure>
+
+    </section>
+
+  </div>
+
+</main>
